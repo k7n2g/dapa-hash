dc3cd43d9b621ae6f25184f1d52d6b877e8397a9
team <qbitcurrency@gmail.com>
2025-05-03 18:24:56 +0100

    update

---
+++ C/README.md
@@ -0,0 +1,7 @@
+```
+git clone --recursive https://github.com/xelis-project/xelis-hash.git
+cd xelis-hash/C
+mkdir build && cd build
+cmake .. 
+make
+```
---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use thiserror::Error as ThisError;
+
+#[cfg(feature = "v1")]
+pub mod v1;
+#[cfg(feature = "v2")]
+pub mod v2;
+
+#[cfg(feature = "tracker")]
+pub mod tracker;
+
+// Number of bytes in a hash
+const HASH_SIZE: usize = 32;
+
+// Hash type alias
+pub type Hash = [u8; HASH_SIZE];
+
+// Error that can occur while hashing
+#[derive(Debug, ThisError)]
+#[error("Error while hashing")]
+pub enum Error {
+    #[error("Error while hashing")]
+    Error,
+    #[error("Error while casting: {0}")]
+    CastError(bytemuck::PodCastError),
+    #[error("Error on format")]
+    FormatError,
+}
+
